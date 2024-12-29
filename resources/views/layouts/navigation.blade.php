506a734ae7aa1280e85978878ae4a279f96dd907
Nihan Cj <nihancj.com@gmail.com>
2024-12-30 01:24:38 +0530

    add navigation bar

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="container">
+        <div class="logo">
+<img src="{{asset('assets/link-logo.png')}}" height=50 alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li class="dropdown"><a href="#"><span>Office Bearers</span></a>
+                    <ul>
+                        <li><a href="https://ieee-link.org/office-bearers/2024">Present</a></li>
+                        <li><a href="https://ieee-link.org/office-bearers/2023">2023</a></li>
+                        <li><a href="https://ieee-link.org/office-bearers/2022">2022</a></li>
+                        <li><a href="https://ieee-link.org/office-bearers/2021">2021</a></li>
+                    </ul>
+                </li>
+                <li><a href="https://drive.google.com/file/d/1tORFd5oBg4c9ve1THdZ64pFeoImRa7Hd/view?usp=sharing" class="" target="_blank" rel="noopener noreferrer">IEEE LINKED</a></li>
+                <li class="dropdown"><a href="#"><span>Resources</span></a>
+                    <ul>
+                        <li><a href="https://ieee-link.org/execom-training">Execom Training</a></li>
+                        <li><a href="https://ieee-link.org/award-awareness">Award Awareness</a></li>
+                        <li><a href="https://ieee-link.org/irl">Communication Partner</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
