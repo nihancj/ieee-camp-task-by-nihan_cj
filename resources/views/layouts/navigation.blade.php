293bc2957195e00c7b4d14cdd3d3a6e592324c28
Nihan Cj <nihancj.com@gmail.com>
2024-12-30 13:47:15 +0530

    update navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,31 +1,11 @@
-<header>
-    <div class="container">
+<!-- <header> -->
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container align-right">
         <div class="logo pb-3">
+ <a class="navbar-brand" href="#">
             <img src="{{ asset('assets/link-logo.png') }}" height=50 alt="">
+</a>
         </div>
-        <!-- <nav> -->
-        <!-- <ul> -->
-        <!--     <li><a href="#home">Home</a></li> -->
-        <!--     <li class="dropdown"><a href="#"><span>Office Bearers</span></a> -->
-        <!--         <ul> -->
-        <!--             <li><a href="https://ieee-link.org/office-bearers/2024">Present</a></li> -->
-        <!--             <li><a href="https://ieee-link.org/office-bearers/2023">2023</a></li> -->
-        <!--             <li><a href="https://ieee-link.org/office-bearers/2022">2022</a></li> -->
-        <!--             <li><a href="https://ieee-link.org/office-bearers/2021">2021</a></li> -->
-        <!--         </ul> -->
-        <!--     </li> -->
-        <!--     <li><a href="https://drive.google.com/file/d/1tORFd5oBg4c9ve1THdZ64pFeoImRa7Hd/view?usp=sharing" class="" target="_blank" rel="noopener noreferrer">IEEE LINKED</a></li> -->
-        <!--     <li class="dropdown"><a href="#"><span>Resources</span></a> -->
-        <!--         <ul> -->
-        <!--             <li><a href="https://ieee-link.org/execom-training">Execom Training</a></li> -->
-        <!--             <li><a href="https://ieee-link.org/award-awareness">Award Awareness</a></li> -->
-        <!--             <li><a href="https://ieee-link.org/irl">Communication Partner</a></li> -->
-        <!--         </ul> -->
-        <!--     </li> -->
-        <!-- </ul> -->
-        <!-- </nav> -->
-        <nav class="navbar navbar-expand-lg text-white">
-            <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -73,5 +53,4 @@
             </div>
         </nav>
 
-</div>
-</header>
+<!-- </header> -->
