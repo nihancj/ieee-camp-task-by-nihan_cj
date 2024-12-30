ae467a1f01bd29356a7412b7ac13f76feefde4e7
Nihan Cj <nihancj.com@gmail.com>
2024-12-30 19:03:49 +0530

    revamp navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,15 +1,51 @@
 <!-- <header> -->
-<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body stiky-top p-3" data-bs-theme="dark">
             <div class="container align-right">
         <div class="logo pb-3">
  <a class="navbar-brand" href="#">
             <img src="{{ asset('assets/link-logo.png') }}" height=50 alt="">
 </a>
         </div>
+            <button class="navbar-toggler m-4" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="1.5em" height="1.5em"
+                        viewBox="0 0 48 48">
+                        <linearGradient id="ULyvxr9k_jV3hToEf4Ujga_6uJdcB0tVRwZ_gr1" x1="12.066" x2="34.891"
+                            y1=".066" y2="22.891" gradientUnits="userSpaceOnUse">
+                            <stop offset=".237" stop-color="#3bc9f3"></stop>
+                            <stop offset=".85" stop-color="#1591d8"></stop>
+                        </linearGradient>
+                        <path fill="url(#ULyvxr9k_jV3hToEf4Ujga_6uJdcB0tVRwZ_gr1)"
+                            d="M43,15H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,14.1,44.1,15,43,15z">
+                        </path>
+                        <linearGradient id="ULyvxr9k_jV3hToEf4Ujgb_6uJdcB0tVRwZ_gr2" x1="12.066" x2="34.891"
+                            y1="12.066" y2="34.891" gradientUnits="userSpaceOnUse">
+                            <stop offset=".237" stop-color="#3bc9f3"></stop>
+                            <stop offset=".85" stop-color="#1591d8"></stop>
+                        </linearGradient>
+                        <path fill="url(#ULyvxr9k_jV3hToEf4Ujgb_6uJdcB0tVRwZ_gr2)"
+                            d="M43,27H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,26.1,44.1,27,43,27z">
+                        </path>
+                        <linearGradient id="ULyvxr9k_jV3hToEf4Ujgc_6uJdcB0tVRwZ_gr3" x1="12.066" x2="34.891"
+                            y1="24.066" y2="46.891" gradientUnits="userSpaceOnUse">
+                            <stop offset=".237" stop-color="#3bc9f3"></stop>
+                            <stop offset=".85" stop-color="#1591d8"></stop>
+                        </linearGradient>
+                        <path fill="url(#ULyvxr9k_jV3hToEf4Ujgc_6uJdcB0tVRwZ_gr3)"
+                            d="M43,39H5c-1.1,0-2-0.9-2-2v-2c0-1.1,0.9-2,2-2h38c1.1,0,2,0.9,2,2v2C45,38.1,44.1,39,43,39z">
+                        </path>
+                    </svg>
+                </span>
+            </button>
+                </div>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel" style="background-color: transparent">
+                <div class="offcanvas-body">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+                            <a class="nav-link text-light" aria-current="page" href="#top">Home</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown"
@@ -45,6 +81,8 @@
                             </ul>
                         </li>
                     </ul>
+                </div>
+                </div>
                     <!-- <form class="d-flex" role="search"> -->
                     <!--   <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="backgroundColor=#fff;"> -->
                     <!--   <button class="btn btn-outline-success" type="submit">Search</button> -->
